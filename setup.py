13454f42dcc3e26fe7f98ae2beeb86406f172dec
Peter Sumbler <peter@gavagai.se>
2019-01-11 14:34:08 +0100

    Setup file initialisation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ParaLex',
+    version='1.0',
+    packages=[''],
+    url='github.com/gavagai-corp/ParaLex',
+    license='Apache License 2.0 ',
+    author='Peter Sumbler',
+    author_email='peter@gavagai.se',
+    description='Evaluation of Word Embeddings'
+)
